c5dd158454aabb3fd0254236dfdcc5c1e608196e
Pinball Wizard <pinball_wizard@criptext.com>
2021-01-05 18:19:13 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+## Uint256 on Cosmwasm math
+
+This is a very early stage idea of extending to Uint256 cosmwasm math file of std_cosmwasm module, the file can be found here:
+
+[https://github.com/CosmWasm/cosmwasm/blob/master/packages/std/src/math.rs](https://)
+
+Rust support natively numbers up to u128 and this cosmwasm library use a wrapper called Decimal to achieve std operation with fixed length math:
+
+```
+const DECIMAL_FRACTIONAL: u128 = 1_000_000_000_000_000_000;
+```
+
+this is the "one" so 1*10^18 which is quite tipical for many ethereum tokens.
+
+We would like to add more "big numbers" natively to this library and so we would like to start with the possibility to have Uint256 numbers with std operations checked with overflows.
+
+Looking around to the huge crates of Rust community I've found a implementation of big uint made by the Rust team:
+
+[https://crates.io/crates/num-bigint](https://)
+
+this is used by other crates to create more specifics numbers management, an interesting one for uint256 is:
+
+[https://crates.io/crates/num256](https://)
+
+which is exactly creating a struct for Uint256 and giving std operations on them.
+
+I think this could be a starting point to add some bigger numbers to cosmwasm std module or maybe just to use in side projects to have less worries about bigger numbers.
+
+Typical problem I suppose we will have to face are:
+
+* Overflows/Underflows
+* Multiplications
+* Zero divide
+
+In the example file I've isolated as a single project the math.rs file adding some examples in main function and test functions of elementary math on some Uint256.
+
+This is just a draft and any comments, advice or ideas are welcome to go on!
+
---
+++ src/errors/std_error.rs
@@ -0,0 +1,371 @@
+#[cfg(feature = "backtraces")]
+use std::backtrace::Backtrace;
+use thiserror::Error;
+
+/// Structured error type for init, handle and query.
+///
+/// This can be serialized and passed over the Wasm/VM boundary, which allows us to use structured
+/// error types in e.g. integration tests. In that process backtraces are stripped off.
+///
+/// The prefix "Std" means "the standard error within the standard library". This is not the only
+/// result/error type in cosmwasm-std.
+///
+/// When new cases are added, they should describe the problem rather than what was attempted (e.g.
+/// InvalidBase64 is preferred over Base64DecodingErr). In the long run this allows us to get rid of
+/// the duplication in "StdError::FooErr".
+///
+/// Checklist for adding a new error:
+/// - Add enum case
+/// - Add creator function in std_error_helpers.rs
+#[derive(Error, Debug)]
+pub enum StdError {
+    /// Whenever there is no specific error type available
+    #[error("Generic error: {msg}")]
+    GenericErr {
+        msg: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("Invalid Base64 string: {msg}")]
+    InvalidBase64 {
+        msg: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("Invalid data size: expected={expected} actual={actual}")]
+    InvalidDataSize {
+        expected: u64,
+        actual: u64,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    /// Whenever UTF-8 bytes cannot be decoded into a unicode string, e.g. in String::from_utf8 or str::from_utf8.
+    #[error("Cannot decode UTF8 bytes into string: {msg}")]
+    InvalidUtf8 {
+        msg: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("{kind} not found")]
+    NotFound {
+        kind: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("Error parsing into type {target_type}: {msg}")]
+    ParseErr {
+        /// the target type that was attempted
+        target_type: String,
+        msg: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("Error serializing type {source_type}: {msg}")]
+    SerializeErr {
+        /// the source type that was attempted
+        source_type: String,
+        msg: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+    #[error("Cannot subtract {subtrahend} from {minuend}")]
+    Underflow {
+        minuend: String,
+        subtrahend: String,
+        #[cfg(feature = "backtraces")]
+        backtrace: Backtrace,
+    },
+}
+
+impl StdError {
+    pub fn generic_err<S: Into<String>>(msg: S) -> Self {
+        StdError::GenericErr {
+            msg: msg.into(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn invalid_base64<S: ToString>(msg: S) -> Self {
+        StdError::InvalidBase64 {
+            msg: msg.to_string(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn invalid_data_size(expected: usize, actual: usize) -> Self {
+        StdError::InvalidDataSize {
+            // Cast is safe because usize is 32 or 64 bit large in all environments we support
+            expected: expected as u64,
+            actual: actual as u64,
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn invalid_utf8<S: ToString>(msg: S) -> Self {
+        StdError::InvalidUtf8 {
+            msg: msg.to_string(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn not_found<S: Into<String>>(kind: S) -> Self {
+        StdError::NotFound {
+            kind: kind.into(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn parse_err<T: Into<String>, M: ToString>(target: T, msg: M) -> Self {
+        StdError::ParseErr {
+            target_type: target.into(),
+            msg: msg.to_string(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn serialize_err<S: Into<String>, M: ToString>(source: S, msg: M) -> Self {
+        StdError::SerializeErr {
+            source_type: source.into(),
+            msg: msg.to_string(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+
+    pub fn underflow<U: ToString>(minuend: U, subtrahend: U) -> Self {
+        StdError::Underflow {
+            minuend: minuend.to_string(),
+            subtrahend: subtrahend.to_string(),
+            #[cfg(feature = "backtraces")]
+            backtrace: Backtrace::capture(),
+        }
+    }
+}
+
+impl From<std::str::Utf8Error> for StdError {
+    fn from(source: std::str::Utf8Error) -> Self {
+        Self::invalid_utf8(source)
+    }
+}
+
+impl From<std::string::FromUtf8Error> for StdError {
+    fn from(source: std::string::FromUtf8Error) -> Self {
+        Self::invalid_utf8(source)
+    }
+}
+
+/// The return type for init, handle and query. Since the error type cannot be serialized to JSON,
+/// this is only available within the contract and its unit tests.
+///
+/// The prefix "Std" means "the standard result within the standard library". This is not the only
+/// result/error type in cosmwasm-std.
+pub type StdResult<T> = core::result::Result<T, StdError>;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::str;
+
+    // constructors
+
+    // example of reporting contract errors with format!
+    #[test]
+    fn generic_err_owned() {
+        let guess = 7;
+        let error = StdError::generic_err(format!("{} is too low", guess));
+        match error {
+            StdError::GenericErr { msg, .. } => {
+                assert_eq!(msg, String::from("7 is too low"));
+            }
+            e => panic!("unexpected error, {:?}", e),
+        }
+    }
+
+    // example of reporting static contract errors
+    #[test]
+    fn generic_err_ref() {
+        let error = StdError::generic_err("not implemented");
+        match error {
+            StdError::GenericErr { msg, .. } => assert_eq!(msg, "not implemented"),
+            e => panic!("unexpected error, {:?}", e),
+        }
+    }
+
+    #[test]
+    fn invalid_base64_works_for_strings() {
+        let error = StdError::invalid_base64("my text");
+        match error {
+            StdError::InvalidBase64 { msg, .. } => {
+                assert_eq!(msg, "my text");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn invalid_base64_works_for_errors() {
+        let original = base64::DecodeError::InvalidLength;
+        let error = StdError::invalid_base64(original);
+        match error {
+            StdError::InvalidBase64 { msg, .. } => {
+                assert_eq!(msg, "Encoded text cannot have a 6-bit remainder.");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn invalid_data_size_works() {
+        let error = StdError::invalid_data_size(31, 14);
+        match error {
+            StdError::InvalidDataSize {
+                expected, actual, ..
+            } => {
+                assert_eq!(expected, 31);
+                assert_eq!(actual, 14);
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn invalid_utf8_works_for_strings() {
+        let error = StdError::invalid_utf8("my text");
+        match error {
+            StdError::InvalidUtf8 { msg, .. } => {
+                assert_eq!(msg, "my text");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn invalid_utf8_works_for_errors() {
+        let original = String::from_utf8(vec![0x80]).unwrap_err();
+        let error = StdError::invalid_utf8(original);
+        match error {
+            StdError::InvalidUtf8 { msg, .. } => {
+                assert_eq!(msg, "invalid utf-8 sequence of 1 bytes from index 0");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn not_found_works() {
+        let error = StdError::not_found("gold");
+        match error {
+            StdError::NotFound { kind, .. } => assert_eq!(kind, "gold"),
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn parse_err_works() {
+        let error = StdError::parse_err("Book", "Missing field: title");
+        match error {
+            StdError::ParseErr {
+                target_type, msg, ..
+            } => {
+                assert_eq!(target_type, "Book");
+                assert_eq!(msg, "Missing field: title");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn serialize_err_works() {
+        let error = StdError::serialize_err("Book", "Content too long");
+        match error {
+            StdError::SerializeErr {
+                source_type, msg, ..
+            } => {
+                assert_eq!(source_type, "Book");
+                assert_eq!(msg, "Content too long");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn underflow_works_for_u128() {
+        let error = StdError::underflow(123u128, 456u128);
+        match error {
+            StdError::Underflow {
+                minuend,
+                subtrahend,
+                ..
+            } => {
+                assert_eq!(minuend, "123");
+                assert_eq!(subtrahend, "456");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn underflow_works_for_i64() {
+        let error = StdError::underflow(777i64, 1234i64);
+        match error {
+            StdError::Underflow {
+                minuend,
+                subtrahend,
+                ..
+            } => {
+                assert_eq!(minuend, "777");
+                assert_eq!(subtrahend, "1234");
+            }
+            _ => panic!("expect different error"),
+        }
+    }
+
+    #[test]
+    fn implements_debug() {
+        let error: StdError = StdError::underflow(3, 5);
+        let embedded = format!("Debug message: {:?}", error);
+        assert_eq!(
+            embedded,
+            r#"Debug message: Underflow { minuend: "3", subtrahend: "5" }"#
+        );
+    }
+
+    #[test]
+    fn implements_display() {
+        let error: StdError = StdError::underflow(3, 5);
+        let embedded = format!("Display message: {}", error);
+        assert_eq!(embedded, "Display message: Cannot subtract 5 from 3");
+    }
+
+    #[test]
+    fn from_std_str_utf8error_works() {
+        let error: StdError = str::from_utf8(b"Hello \xF0\x90\x80World")
+            .unwrap_err()
+            .into();
+        match error {
+            StdError::InvalidUtf8 { msg, .. } => {
+                assert_eq!(msg, "invalid utf-8 sequence of 3 bytes from index 6")
+            }
+            err => panic!("Unexpected error: {:?}", err),
+        }
+    }
+
+    #[test]
+    fn from_std_string_fromutf8error_works() {
+        let error: StdError = String::from_utf8(b"Hello \xF0\x90\x80World".to_vec())
+            .unwrap_err()
+            .into();
+        match error {
+            StdError::InvalidUtf8 { msg, .. } => {
+                assert_eq!(msg, "invalid utf-8 sequence of 3 bytes from index 6")
+            }
+            err => panic!("Unexpected error: {:?}", err),
+        }
+    }
+}
